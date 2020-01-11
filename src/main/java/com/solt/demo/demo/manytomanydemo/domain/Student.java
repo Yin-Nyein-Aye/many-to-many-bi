098fd4a06eadef217f05fbf7131ecc81aac2dbe4
Yin-Nyein-Aye <yinnyeinaye.yinnyein.com>
2020-01-11 21:13:53 +0630

    many to many bi

---
+++ src/main/java/com/solt/demo/demo/manytomanydemo/domain/Student.java
@@ -0,0 +1,79 @@
+package com.solt.demo.demo.manytomanydemo.domain;
+import javax.persistence.*;
+import java.util.List;
+import java.util.ArrayList;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private  String name;
+    private  int age;
+    private  String school;
+
+    @ManyToMany(cascade = CascadeType.PERSIST)
+    private List<Subject> subjects= new ArrayList<>();
+
+    public Student() {
+    }
+
+    public Student(String name, int age, String school) {
+        this.name = name;
+        this.age = age;
+        this.school = school;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public List<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(List<Subject> subjects) {
+        this.subjects = subjects;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", school='" + school + '\'' +
+                ", subjects=" + subjects +
+                '}';
+    }
+}
+
+
